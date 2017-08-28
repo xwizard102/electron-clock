12e02b830f515f8071adc42a60a4dd710a9c5961
Dr. Neil Brittliff <Neil.Brittliff@homemail.com.au>
2017-08-28 14:55:02 +1000

    Hidden Menu Bar

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 417, height: 442, resizable: false})
+  mainWindow = new BrowserWindow({width: 417, height: 442, resizable: false, autoHideMenuBar: true})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
