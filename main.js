363cce7591c6d0c3aa0e08b4f2e19d99648ac062
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2018-05-19 23:49:51 +1000

    Updated App.js

--- main.js
+++ main.js
@@ -20,18 +20,18 @@ function createWindow () {
     slashes: true
   }))
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed',  () => {
     mainWindow = null
   })
 }
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed',  () => {
   app.quit()
 })
 
-app.on('activate', function () {
+app.on('activate',  () => {
   if (mainWindow === null) {
     createWindow()
   }
