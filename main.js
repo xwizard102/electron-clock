0bc92cb88ff1c2bc90436e2803521ac23ba6db83
Dr. Neil Brittliff <Neil.Brittliff@homemail.com.au>
2018-09-03 22:56:32 +1000

    Updated UI

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var mainWindow = null;
 
 function createWindow () {
 
-  mainWindow = new BrowserWindow({width: 158, height: 130, 
+  mainWindow = new BrowserWindow({width: 138, height: 120, 
                                   resizable: false, 
                                   frame: false,
                                   transparent: true, 
